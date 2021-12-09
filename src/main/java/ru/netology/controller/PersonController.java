6538dd15c160c33cadbf79241424945febdad324
frepingod <frepingod@gmail.com>
2021-12-09 20:43:13 +0300

    dao with jpa repository

--- src/main/java/ru/netology/controller/PersonController.java
+++ src/main/java/ru/netology/controller/PersonController.java
@@ -2,21 +2,34 @@ package ru.netology.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.model.Person;
 import ru.netology.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/persons")
 public class PersonController {
 
     private PersonRepository personRepository;
 
-    @GetMapping("/persons/by-city")
-    public List<Person> getPersonsByCity(@RequestParam String city) {
-        return personRepository.getPersonsByCity(city);
+    @GetMapping("/by-city")
+    public List<Person> findByCityOfLiving(@RequestParam String city) {
+        return personRepository.findByCityOfLiving(city);
+    }
+
+    @GetMapping("/by-age")
+    public List<Person> findByAgeLessThanOrderByAge(@RequestParam int age) {
+        return personRepository.findByAgeLessThanOrderByAge(age);
+    }
+
+    @GetMapping("/by-name-and-surname")
+    public Optional<Person> findByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
+        return personRepository.findByNameAndSurname(name, surname);
     }
 }
