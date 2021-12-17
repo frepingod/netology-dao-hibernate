d9956f76f90bac974645b231174685d9cf4bbfc4
frepingod <frepingod@gmail.com>
2021-12-17 20:37:18 +0300

    jpa security methods

--- src/main/java/ru/netology/config/PersonConfig.java
+++ src/main/java/ru/netology/config/PersonConfig.java
@@ -1,17 +1,31 @@
 package ru.netology.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.netology.model.Roles;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class PersonConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and()
-                .authorizeRequests().antMatchers("/persons/all").permitAll()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("Ivan").password(encoder.encode("1234")).roles(Roles.DELETE.name())
                 .and()
-                .authorizeRequests().anyRequest().authenticated();
+                .withUser("Oleg").password(encoder.encode("4321")).roles(Roles.WRITE.name())
+                .and()
+                .withUser("Test").password(encoder.encode("1111")).roles(Roles.READ.name());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and().authorizeRequests().anyRequest().authenticated();
     }
 }
--- src/main/java/ru/netology/controller/PersonController.java
+++ src/main/java/ru/netology/controller/PersonController.java
@@ -1,6 +1,8 @@
 package ru.netology.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.netology.model.Person;
 import ru.netology.repository.PersonRepository;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,21 +22,25 @@ public class PersonController {
     private PersonRepository personRepository;
 
     @GetMapping("/by-city")
+    @Secured("ROLE_READ")
     public List<Person> findByCityOfLiving(@RequestParam String city) {
         return personRepository.findByCityOfLiving(city);
     }
 
     @GetMapping("/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> findByAgeLessThanOrderByAge(@RequestParam int age) {
         return personRepository.findByAgeLessThanOrderByAge(age);
     }
 
     @GetMapping("/by-name-and-surname")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public Optional<Person> findByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
         return personRepository.findByNameAndSurname(name, surname);
     }
 
     @GetMapping("/all")
+    @PreAuthorize("authentication.principal.username == 'Ivan'")
     public List<Person> findAll() {
         return personRepository.findAll();
     }
